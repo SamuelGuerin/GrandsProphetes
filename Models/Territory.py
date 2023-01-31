ae440a4a0c34db04a81483826ccb847ecb9d62e8
Laurent Brochu <2029155@etu.cegepsth.qc.ca>
2023-01-31 12:39:23 -0500

    remove territory

--- Models/Territory.py
+++
@@ -1,7 +0,0 @@
-class Territory:
-    def __init__(self,sizeX,sizeY, luluCount, foodCount,map):
-        self.sizeX = sizeX
-        self.sizeY = sizeY
-        self.luluCount = luluCount
-        self.foodCount = foodCount
-        self.map = map
