5336d945cc93956797bdfcacded00d9d8b5fc02c
Laurent Brochu <2029155@etu.cegepsth.qc.ca>
2023-01-31 12:37:43 -0500

    Ajout Models

---
+++ Models/Position.py
@@ -0,0 +1,16 @@
+class Position:
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+
+    def __eq__(self, other) -> bool:
+        return (self.x, self.y) == (other.x, other.y)
+
+    def __ne__(self, other) -> bool:
+        return not(self == other)
+
+    def __repr__(self) -> str:
+        return ('position (' + str(self.x) + ', ' + str(self.y) + ')')
---
+++ Models/Territory.py
@@ -0,0 +1,7 @@
+class Territory:
+    def __init__(self,sizeX,sizeY, luluCount, foodCount,map):
+        self.sizeX = sizeX
+        self.sizeY = sizeY
+        self.luluCount = luluCount
+        self.foodCount = foodCount
+        self.map = map
