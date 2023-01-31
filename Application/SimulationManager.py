7b256b71e2552e3e3f0a1a94b82afdeaf26a9e42
Samuel Guérin <1976256@etu.cegepsth.qc.ca>
2023-01-31 17:59:38 -0500

    Ajout des générations

--- Application/SimulationManager.py
+++ Application/SimulationManager.py
@@ -1,8 +1,11 @@
 import Models.Territory as Territory
 
-def __run__(sizeX, sizeY, foodCount, lulusCount, speed, sense, size, energy):    
-    Territory.createMap(sizeX, sizeY, foodCount, lulusCount, speed, sense, energy, 0, size)
-    
-    Territory.moveAll()
-    
-    Territory.dayResultLulu()
+
+def __run__(sizeX, sizeY, foodCount, lulusCount, speed, sense, size, energy, nbGeneration):
+    Territory.createMap(sizeX, sizeY, foodCount, lulusCount,
+                        speed, sense, energy, 0, size)
+
+    for generation in range(nbGeneration):
+
+        Territory.moveAll()
+        Territory.dayResultLulu()
