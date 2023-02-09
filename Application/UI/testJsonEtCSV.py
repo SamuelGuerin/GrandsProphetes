457835259f1bd14481139b90550cdb8718fef829
RaphS <89414388+RaphS@users.noreply.github.com>
2023-02-09 18:02:24 -0500

    Algo pour trier les bons fichier des mauvais début

--- Application/UI/testJsonEtCSV.py
+++ Application/UI/testJsonEtCSV.py
@@ -1,6 +1,8 @@
 import json
 import FormGraph as fg
 from pathlib import Path
+from os import listdir
+import re
 
 generationsObjects = fg.generateLulus()
 generations = []
@@ -23,11 +25,27 @@ for generation in generationsObjects:
 def obj_dict(obj):
     return obj.__dict__
 
+def fileNumber(name):
+    match = re.match("^save([1-9][0-9]{0,9}).json", name)
+    return int(match.group(1))
+
 jsonString = json.dumps(generations, default=obj_dict)
 
 Path("Save/").mkdir(parents=True, exist_ok=True)
 
-f = open("Save/save1.json", "w")
+filenames = listdir("Save/")
+
+reggex = re.compile("^save[1-9][0-9]{0,9}.json")
+filenames = [name for name in filenames if reggex.match(name)]
+
+filenames = sorted(filenames, key=fileNumber)
+
+if (len(filenames) == 0):
+    test = 1
+else:
+    test = fileNumber(filenames[len(filenames) - 1]) + 1
+
+f = open("Save/save" + str(test) + ".json", "w")
 f.write(jsonString)
 f.close()
 
